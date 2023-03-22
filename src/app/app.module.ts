4b916345b75f6c0917922bff5da8409e9e1a8cea
joaoabjr <junioralmeida_15@hotmail.com>
2023-03-22 13:39:20 -0300

    att btn and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NewMomentComponent } from './pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { EquipeComponent } from './pages/equipe/equipe.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +22,15 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     AboutComponent,
     HomeComponent,
     NewMomentComponent,
-    MomentFormComponent
+    MomentFormComponent,
+    EquipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent {
-
+  @Input() btnText!: string; //passado pelo componente pai (onde chama o moment-form, que nesse caso é o new-moment)
 }
